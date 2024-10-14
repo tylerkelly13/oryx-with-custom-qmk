e7bc92172f0a59940740a4753dd3f44db46d7404
Matt Patterson <mattpatterson@canva.com>
2024-10-14 21:57:34 +1000

    remove matt's layout

--- mmzqD/rules.mk
+++
@@ -1,8 +0,0 @@
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-SPACE_CADET_ENABLE = no
-CAPS_WORD_ENABLE = yes
-LAYER_LOCK_ENABLE = yes
-SRC += features/achordion.c
